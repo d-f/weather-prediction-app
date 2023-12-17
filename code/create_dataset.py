61d3c2990c26153d8b9262658788f3f571d153cb
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2023-12-17 13:13:16 -0700

    Update create_dataset.py

--- code/create_dataset.py
+++ code/create_dataset.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 from pathlib import Path
 import pymongo 
@@ -9,9 +10,10 @@ import utils
 def create_argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-host_name", default="localhost:27017/")
-    parser.add_argument("-db_name", default="noaa_global_marine")
+    parser.add_argument("-db_name", default="noaa_global_hourly")
     parser.add_argument("-col_name", default="wp_app")
     parser.add_argument("-save_path", default="C:\\personal_ML\\weather_prediction_app\\raw_dataset.json")
+    parser.add_argument("-threshold", type=int, default=20)
     return parser.parse_args()
 
 
@@ -28,26 +30,33 @@ def get_collection(database, collection_name: str):
     return database[collection_name]
 
 
-def get_stations(collection):
-    return collection.distinct("STATION")
+def get_dates(collection):
+    return collection.distinct("DATE")
 
 
-def create_dataset(collection, stations):
-    dataset = {x: {"WET_BULB_TEMP":[],"VISIBILITY":[],"WIND_SPEED":[],"DATE":[]} for x in stations}
-    for station_name in tqdm(dataset.keys()):
+def create_dataset(collection, dates):
+    dataset = {}
+    for date in tqdm(dates):
         for doc in collection.find(
+            # get all documents with ,1 in the TMP measurement and a TMP != +9999
             {
-            "WET_BULB_TEMP": {"$ne": ""}, 
-            "VISIBILITY": {"$ne": ""},
-            "WIND_SPEED": {"$ne": ""},
-            "STATION": station_name
+            "DATE": date,
+            "TMP": "/,1/",
+            "TMP": {"$ne": "+9999"} 
             }
-            ).sort("DATE", 1):
-            dataset[station_name]["WET_BULB_TEMP"].append(doc["WET_BULB_TEMP"])
-            dataset[station_name]["VISIBILITY"].append(doc["VISIBILITY"])
-            dataset[station_name]["WIND_SPEED"].append(doc["WIND_SPEED"])
-            dataset[station_name]["DATE"].append(doc["DATE"])
-    dataset = {x: y for x, y in dataset.items() if len(y["WET_BULB_TEMP"]) > 0}
+        ):
+            year = int(doc["DATE"].partition("-")[0])
+            month = int(doc["DATE"].partition("-")[2].partition("-")[0])
+            day = int(doc["DATE"].partition("-")[2].partition("-")[2].partition("T")[0])
+            hour = int(doc["DATE"].partition("T")[2].partition(":")[0])
+            minute = int(doc["DATE"].rpartition(":")[0].rpartition(":")[2])
+            dt = datetime(year, month, day, hour, minute)
+            dt.replace(hour=hour, minute=minute)
+            temp = int(doc["TMP"].partition(",")[0][1:])
+            if "-" == doc["TMP"][0]:
+                temp *= -1
+            dataset[dt.timestamp()] = temp
+
     return dataset
 
 
@@ -61,8 +70,8 @@ def main():
     client = utils.get_client(host_name=args.host_name)
     database = utils.get_database(client=client, database_name=args.db_name)
     collection = utils.get_collection(database=database, collection_name=args.col_name)
-    stations = get_stations(collection=collection)
-    station_dict = create_dataset(collection=collection, stations=stations)
+    dates = get_dates(collection=collection)
+    station_dict = create_dataset(collection=collection, dates=dates)
     save_json(json_dict=station_dict, file_path=Path(args.save_path))
 
 
