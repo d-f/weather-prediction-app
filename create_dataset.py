956ee7e5c5d1fd5526883028c407c13fcc5d1bcc
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-03-21 14:24:58 -0600

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -5,35 +5,27 @@ import pymongo
 import argparse
 from tqdm import tqdm
 import utils
+from typing import Type
 
 
-def create_argparser():
+def parser_argparser():
+    """return command-line argument parser"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-host_name", default="localhost:27017/")
-    parser.add_argument("-db_name", default="noaa_global_hourly")
-    parser.add_argument("-col_name", default="wp_app")
-    parser.add_argument("-save_path", default="C:\\personal_ML\\weather_prediction_app\\raw_dataset.json")
+    parser.add_argument("-host_name", type=str)
+    parser.add_argument("-db_name", type=str)
+    parser.add_argument("-col_name", type=str)
+    parser.add_argument("-save_path", type=Path)
     return parser.parse_args()
 
 
-def get_client(host_name: str):
-    client = pymongo.MongoClient(f"mongodb://{host_name}")
-    return client
+def create_dataset(collection: Type[pymongo.collection], dates: list):
+    """
+    creates dataset from pymongo collection
 
-
-def get_database(client, database_name: str):
-    return client[database_name]
-
-
-def get_collection(database, collection_name: str):
-    return database[collection_name]
-
-
-def get_dates(collection):
-    return collection.distinct("DATE")
-
-
-def create_dataset(collection, dates):
+    keyword arguments:
+    collection -- pymongo collection
+    dates      -- list result from dataset_dates() in /utils/data_utils.py
+    """
     dataset = {}
     for date in tqdm(dates):
         for doc in collection.find(
@@ -58,19 +50,14 @@ def create_dataset(collection, dates):
     return dataset
 
 
-def save_json(json_dict, file_path: Path):
-    with open(file_path, mode="w") as opened_json:
-        json.dump(json_dict, opened_json)    
-
-
 def main():
-    args = create_argparser()
-    client = utils.get_client(host_name=args.host_name)
-    database = utils.get_database(client=client, database_name=args.db_name)
-    collection = utils.get_collection(database=database, collection_name=args.col_name)
-    dates = get_dates(collection=collection)
+    args = parser_argparser()
+    client = utils.data_utils.mongo_client(host_name=args.host_name)
+    database = utils.data_utils.mongo_database(client=client, database_name=args.db_name)
+    collection = utils.data_utils.db_collection(database=database, collection_name=args.col_name)
+    dates = utils.data_utils.dataset_dates(collection=collection)
     station_dict = create_dataset(collection=collection, dates=dates)
-    save_json(json_dict=station_dict, file_path=Path(args.save_path))
+    utils.data_utils.save_json(json_dict=station_dict, file_path=Path(args.save_path))
 
 
 if __name__ == "__main__":
