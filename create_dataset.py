6b356d1a9efc0e905ca41390450f4b3dba2a0d35
Daniel Franklin <118086192+d-f@users.noreply.github.com>
2024-05-12 23:29:40 -0600

    Update method name

--- create_dataset.py
+++ create_dataset.py
@@ -10,9 +10,9 @@ from typing import Type
 def parser_argparser():
     """return command-line argument parser"""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-host_name", type=str)
-    parser.add_argument("-db_name", type=str)
-    parser.add_argument("-col_name", type=str)
+    parser.add_argument("-host_name", type=str, default="localhost:27017/")
+    parser.add_argument("-db_name", type=str, default="noaa_global_hourly")
+    parser.add_argument("-col_name", type=str, default="wp_app")
     parser.add_argument("-save_path", type=Path)
     return parser.parse_args()
 
@@ -53,7 +53,7 @@ def main():
     args = parser_argparser()
     client = utils.data_utils.mongo_client(host_name=args.host_name)
     database = utils.data_utils.mongo_database(client=client, database_name=args.db_name)
-    collection = utils.data_utils.db_collection(database=database, collection_name=args.col_name)
+    collection = utils.data_utils.mongo_collection(database=database, collection_name=args.col_name)
     dates = utils.data_utils.dataset_dates(collection=collection)
     station_dict = create_dataset(collection=collection, dates=dates)
     utils.data_utils.save_json(json_dict=station_dict, file_path=Path(args.save_path))
